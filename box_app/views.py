61b342288c7ab7c51972371cb5d33e50716c456c
MRBOX <olamigokeadebayo90@gmail.com>
2024-06-24 23:05:38 +0100

    Handled the Signup and login

--- box_app/views.py
+++ box_app/views.py
@@ -1,7 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUPForm, LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'base/home.html')
+
+
+def Signup(request):
+    if request.method == 'POST':
+        form = SignUPForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            email = form.cleaned_data.get('email')
+            
+            user = authenticate(username=username, password=raw_password)
+            
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Welcome, {username}! Your account was successfully created.")
+                return redirect('home')  # Redirect to a home or profile page
+            else:
+                messages.error(request, "Authentication failed. Please try again.")
+        else:
+            # Form is invalid, display errors
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{field}: {error}")
+
+    else:
+        form = SignUPForm()
+
+    return render(request, 'base/register.html', {'form': form})
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        
+        user = authenticate(request, email=email, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful")
+            return redirect('home')
+        else:
+            messages.error(request, "Username does not exist")
+            
+    return render(request, 'base/register.html')
